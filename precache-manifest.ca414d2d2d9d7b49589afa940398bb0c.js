86d454d438b5efe0cb9f8f7db2fb90162228dc31
Snigdha Reddy Pulim <snigdhapulim2@gmail.com>
2019-11-10 23:59:30 +0530

    Updates

---
+++ precache-manifest.ca414d2d2d9d7b49589afa940398bb0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c7b0af33cb16001dbe35e8938805cb",
+    "url": "/cookies_authentication/index.html"
+  },
+  {
+    "revision": "0ec214ea79c4ba2c1ba9",
+    "url": "/cookies_authentication/static/css/main.e89b29ec.chunk.css"
+  },
+  {
+    "revision": "776b4beb19aa0652041b",
+    "url": "/cookies_authentication/static/js/2.5a3fa9f7.chunk.js"
+  },
+  {
+    "revision": "0ec214ea79c4ba2c1ba9",
+    "url": "/cookies_authentication/static/js/main.a38308fd.chunk.js"
+  },
+  {
+    "revision": "e2d5e811ff94bc39da3e",
+    "url": "/cookies_authentication/static/js/runtime-main.fb02dcb0.js"
+  }
+]);
